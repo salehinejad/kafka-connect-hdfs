29b3e84eea9ec409546e3fa4c04dc8444c599808
pmx <tiny1990@126.com>
2016-10-31 15:45:02 +0800

    fix

--- src/main/java/io/confluent/connect/hdfs/FileUtils.java
+++ src/main/java/io/confluent/connect/hdfs/FileUtils.java
@@ -152,7 +152,7 @@ public class FileUtils {
   }
 
   public static long extractOffset(String filename) {
-    return Long.parseLong(filename.split(HdfsConfig.FILE_EXTENSION)[3]);
+    return Long.parseLong(filename.split(HdfsConfig.DP_COMMMITTED_FILENAME_SEPARATOR_REGEX)[3]);
   }
 
   private static ArrayList<FileStatus> getDirectoriesImpl(Storage storage, Path path)
